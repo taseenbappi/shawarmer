7ff100e7ca0736603ba2f946ce618c7295af9386
taseenbappi <taseen.bappi@gmail.com>
2021-12-24 14:54:10 +0600

    tailwindCSS and daisyUI componenet library setup , setting project structure

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome Bit SOft</h1>
+            <button class="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">Tailwind Button</button>
+        </div>
+    );
+};
+
+export default Home;
