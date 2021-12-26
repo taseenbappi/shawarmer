6b5f60b845de9c1bb64364253534f13ac9211da5
taseenbappi <taseen.bappi@gmail.com>
2021-12-27 01:05:10 +0600

    article card added and readme file edited

--- src/Pages/Home/ArticleContainer/Articles/Articles.js
+++ src/Pages/Home/ArticleContainer/Articles/Articles.js
@@ -5,62 +5,70 @@ const Articles = () => {
     return (
         <section className="text-gray-600 body-font py-10">
 
-            <h1 className='text-3xl font-bold'>Articles</h1>
+            <h1 className='text-3xl font-bold'>Articles Catagories</h1>
+            <div className='py-5'>
+
+                <button class="btn btn-outline btn-primary m-2 capitalize">primary</button>
+                <button class="btn btn-outline btn-primary m-2 capitalize">primary</button>
+                <button class="btn btn-outline btn-primary m-2 capitalize">primary</button>
+                <button class="btn btn-outline btn-primary m-2 capitalize">primary</button>
+
+            </div>
 
             <div className="container px-5 py-24 mx-auto">
 
-                <div className="flex flex-wrap -m-4">
+                <div className="flex flex-wrap -m-4 items-center justify-center">
 
-                    <div className="p-4 lg:w-1/3 ">
-                        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg shadow-indigo-500/40">
-                            <div className='flex justify-between'>
-                                <span className='border-2  bg-red-600 p-2 rounded-lg text-white'>25</span>
-                                <h2 className="tracking-widest  title-font font-medium text-black mb-1">CATEGORY NAME</h2>
+                    <div class="card text-center shadow-2xl w-72 m-5">
+                        <figure class="px-5 pt-5">
+                            <span className='text-xl bg-red-500 text-white p-1 rounded-md '>25</span>
+                            <h2 className='text-xl'>Catagories</h2>
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Articles</h2>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <div class="justify-center card-actions">
+                                <button class="btn btn-outline btn-primary capitalize">More <FaArrowRight className='m-2'></FaArrowRight></button>
                             </div>
-                            <ul className='py-8'>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-
-                            </ul>
-                            <button className="btn btn-outline btn-primary">More <FaArrowRight className='mx-1'></FaArrowRight></button>
-                        </div>
-                    </div>
-                    <div className="p-4 lg:w-1/3 ">
-                        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg shadow-indigo-500/40">
-                            <div className='flex justify-between'>
-                                <span className='border-2  bg-red-600 p-2 rounded-lg text-white'>25</span>
-                                <h2 className="tracking-widest  title-font font-medium text-black mb-1">CATEGORY NAME</h2>
-                            </div>
-                            <ul className='py-8'>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-
-                            </ul>
-                            <button className="btn btn-outline btn-primary">More <FaArrowRight className='mx-1'></FaArrowRight></button>
-                        </div>
-                    </div>
-                    <div className="p-4 lg:w-1/3 ">
-                        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg shadow-indigo-500/40">
-                            <div className='flex justify-between'>
-                                <span className='border-2  bg-red-600 p-2 rounded-lg text-white'>25</span>
-                                <h2 className="tracking-widest  title-font font-medium text-black mb-1">CATEGORY NAME</h2>
-                            </div>
-                            <ul className='py-8'>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-                                <li>blog-1</li>
-
-                            </ul>
-                            <button className="btn btn-outline btn-primary">More <FaArrowRight className='mx-1'></FaArrowRight></button>
                         </div>
                     </div>
 
+                    <div class="card text-center shadow-2xl w-72 m-5">
+                        <figure class="px-5 pt-5">
+                            <span className='text-xl bg-red-500 text-white p-1 rounded-md '>25</span>
+                            <h2 className='text-xl'>Catagories</h2>
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Articles</h2>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <div class="justify-center card-actions">
+                                <button class="btn btn-outline btn-primary capitalize">More <FaArrowRight className='m-2'></FaArrowRight></button>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div class="card text-center shadow-2xl w-72 m-5">
+                        <figure class="px-5 pt-5">
+                            <span className='text-xl bg-red-500 text-white p-1 rounded-md '>25</span>
+                            <h2 className='text-xl'>Catagories</h2>
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Articles</h2>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <p>Article - 01</p>
+                            <div class="justify-center card-actions">
+                                <button class="btn btn-outline btn-primary capitalize">More <FaArrowRight className='m-2'></FaArrowRight></button>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
